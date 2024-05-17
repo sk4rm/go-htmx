c0bdd4cff0493faae3bddf037474d8be693b5765
sk4rm <74897601+sk4rm@users.noreply.github.com>
2024-05-17 16:18:42 -0700

    Use 1.22 routing instead of method switch-case Split new-post template into smaller templates

--- handlers.go
+++ handlers.go
@@ -7,65 +7,118 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/jackc/pgx/v5"
 )
 
 func check(err error) {
 	if err != nil {
-		log.Panicln(err)
+		log.Fatalln(err)
 	}
 }
 
+// POST /posts/
+//
+// Creates a new post and returns a newly generated id.
 func postHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	// GET /posts/<id>
-	//  Shows post with matching id
-	case http.MethodGet:
-		id := r.URL.Path[len("/posts/"):]
-		log.Println("GET /posts/" + id)
+	log.Println("POST /posts/")
 
-		conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	title := r.PostFormValue("title")
+	body := r.PostFormValue("body")
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	check(err)
+	defer conn.Close(context.Background())
+
+	query := "INSERT INTO public.posts (title, body) VALUES ($1::text, $2::text) returning id;"
+	var postID string
+
+	// Create new record in database.
+	err = conn.QueryRow(context.Background(), query, title, body).Scan(&postID)
+	check(err)
+
+	log.Printf("created post #%s\n", postID)
+
+	w.Header().Set("HX-Redirect", "/posts/"+postID)
+}
+
+// GET /posts/{id}
+//
+// Shows post with matching id.
+func viewPostHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	log.Println("GET /posts/" + id)
+
+	_, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, "post id must only contain numbers", http.StatusBadRequest)
+		return
+	}
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	check(err)
+
+	query := "SELECT * FROM posts WHERE id=$1"
+	var postID, title, body string
+	err = conn.QueryRow(context.Background(), query, id).Scan(&postID, &title, &body)
+
+	if err == pgx.ErrNoRows {
+		fmt.Fprintln(w, "no post with id "+id)
+	} else {
 		check(err)
-		defer conn.Close(context.Background())
-
-		query := "SELECT * FROM posts WHERE id=" + id
-		var post_id, title, body string
-		err = conn.QueryRow(context.Background(), query).Scan(&post_id, &title, &body)
-
-		if err == pgx.ErrNoRows {
-			fmt.Fprintln(w, "no post with id "+id)
-		} else {
-			check(err)
-			fmt.Fprintf(w, "post %s:\ntitle: %s\nbody: %s", post_id, title, body)
-		}
-
-	// POST /posts/
-	//  Creates a new post and returns a unique id
-	case http.MethodPost:
-		log.Println("POST /posts/")
-
-		title := r.PostFormValue("title")
-		body := r.PostFormValue("body")
-
-		conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-		check(err)
-		defer conn.Close(context.Background())
-
-		query := "INSERT INTO public.posts (title, body) VALUES ($1::text, $2::text) returning id;"
-		var post_id string
-		err = conn.QueryRow(context.Background(), query, title, body).Scan(&post_id)
-		check(err)
-
-		log.Printf("post #%s successfully created\n", post_id)
-		w.Header().Set("HX-Redirect", "/posts/"+post_id)
-		fmt.Fprintf(w, "post #%s successfully created\n", post_id)
+		fmt.Fprintf(w, "post %s:\ntitle: %s\nbody: %s", postID, title, body)
 	}
 }
 
+// GET /posts/new/
+//
+// Displays frontend for creating new posts.
 func newPostHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/new-post.html")
-	check(err)
+	log.Println("GET /posts/new/")
+
+	tmpl, err := template.ParseFiles("templates/base.html", "templates/new-post.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
 	tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// GET /posts/
+//
+// Display all posts
+func viewAllPostsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET /posts/")
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	check(err)
+	defer conn.Close(context.Background())
+
+	query := "SELECT * FROM posts"
+	rows, err := conn.Query(context.Background(), query)
+	check(err)
+	defer rows.Close()
+
+	isEmpty := true
+	for rows.Next() {
+		isEmpty = false
+
+		var postID, title, body string
+		err := rows.Scan(&postID, &title, &body)
+		check(err)
+
+		_, err = fmt.Fprintf(w, "<a href=\"/posts/%v\">%v</a><br><br>", postID, title)
+		check(err)
+	}
+	check(rows.Err())
+
+	if isEmpty {
+		fmt.Fprintln(w, "no posts :<")
+		fmt.Fprintln(w, "make one at /posts/new? :D")
+	}
 }
--- main.go
+++ main.go
@@ -10,14 +10,20 @@ import (
 )
 
 func main() {
+	if os.Getenv("DATABASE_URL") == "" {
+		log.Fatalln("database url not provided")
+	}
 
 	// Test database connection.
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	check(err)
 	defer conn.Close(context.Background())
 
-	http.HandleFunc("/posts/", postHandler)
-	http.HandleFunc("/posts/new/", newPostHandler)
+	// https://go.dev/blog/routing-enhancements
+	http.HandleFunc("GET /posts/{id}", viewPostHandler)
+	http.HandleFunc("GET /posts/new/", newPostHandler)
+	http.HandleFunc("GET /posts/", viewAllPostsHandler)
+	http.HandleFunc("POST /posts/", postHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
